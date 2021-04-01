5dc8ecb88833114f613195d1a011b80250a3065b
Winston Tokuhisa <tokuhisawinston@outlook.com>
2021-04-01 18:58:12 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -32,14 +32,6 @@ end
 #   applied
 # end
 
-# def apply_clearance(cart)
-#   # code here
-# end
-
-# def checkout(cart, coupons)
-#   # code here
-# end
-
 def apply_coupons(cart, coupons)
   # code here
   coupons.each do | coupon |
@@ -66,6 +58,14 @@ def apply_coupons(cart, coupons)
   
   cart
 end
+
+# def apply_clearance(cart)
+#   # code here
+# end
+
+# def checkout(cart, coupons)
+#   # code here
+# end
 
 def apply_clearance(cart)
   # code here
