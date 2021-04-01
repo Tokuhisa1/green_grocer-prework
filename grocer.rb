3d932135e8bd99f085e8872a5bbf8d06944c9622
Winston Tokuhisa <tokuhisawinston@outlook.com>
2021-04-01 19:02:54 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -29,7 +29,7 @@ def apply_coupons(cart, coupons)
       cart[item][:count] -= coupon[:num]
       
       if cart["#{item} W/COUPON"]
-        cart["#{item} W/COUPON"][:count] += coupon[:num]
+        cart["#{item} W/COUPON"][:count] += 1
       else
         cart["#{item} W/COUPON"] = {
           :price => coupon[:cost],
